9bf32f310643896e07da9c483bf748e76d65e12c
matthew <dontalwayshere@outlook.com>
2022-05-22 07:18:57 +0800

    Java 集成 Google Authenticator

--- cloudalibaba-provider-payment9003/src/main/java/com/matthew/practice/PaymentMain9003.java
+++ cloudalibaba-provider-payment9003/src/main/java/com/matthew/practice/PaymentMain9003.java
@@ -1,14 +1,20 @@
 package com.matthew.practice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.ConfigurableEnvironment;
 
-
+@Slf4j
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PaymentMain9003 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain9003.class, args);
+        ConfigurableEnvironment env = SpringApplication
+                .run(PaymentMain9003.class, args).getEnvironment();
+        String port = env.getProperty("server.port", "8080");
+        log.info("项目信息 http://localhost:{}", port);
+        log.info("二维码 http://localhost:{}/qr_code", port);
     }
 }
